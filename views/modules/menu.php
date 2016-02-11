b17b13e1c47ca3acf25e950bce9dace75a49c6a4
Raciz <1530071@upv.edu.mx>
2016-02-11 10:31:46 -0600

    menu

--- views/modules/menu.php
+++ views/modules/menu.php
@@ -1,7 +1,12 @@
 <!-- Left Menu Start -->
 <ul class="metisMenu nav" id="side-menu">
-    <li><a href="index.html"><i class="ti-home"></i> Dashboard </a></li>
+    <li><a href="index.php?section=dashboard"><i class="ti-home"></i> Dashboard </a></li>
+    <li><a href="index.php?section=usuario&action=listado"><i class="mdi mdi-account-circle"></i> Usuarios </a></li>
+    <li><a href="index.php?section=grupo&action=listado"><i class="mdi mdi-account-multiple"></i> Grupos </a></li>
+    <li><a href="index.php?section=carrera&action=listado"><i class="fa fa-user"></i> Carreras </a></li>
+    <li><a href="index.php?section=alumno&action=listado"><i class="fa fa-user"></i> Alumnos </a></li>
 
+    <!--
     <li>
         <a href="javascript: void(0);" aria-expanded="true"><i class="ti-light-bulb"></i> Components <span class="fa arrow"></span></a>
         <ul class="nav-second-level nav" aria-expanded="true">
@@ -24,4 +29,5 @@
             </li>
         </ul>
     </li>
+-->
 </ul>
