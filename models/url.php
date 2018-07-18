8f46cfd4c59cb3acad43fca38dcea7749becfcaf
Raciz <1530071@upv.edu.mx>
2018-07-18 12:07:12 -0500

    url

--- models/url.php
+++ models/url.php
@@ -6,7 +6,15 @@ class url
     public static function urlModel($section,$action)
     {
         //en caso de que se mande un link valido se redirecciona a su pagina correspondiene
-        if(($section == "careers" || $section == "students" || $section == "users" || $section == "groups" || $section == "activities" || $section == "units"  || $section == "sessions") && ($action == "add" || $action == "list" || $action == "delete" || $action == "edit" || $action == "students" || $action == "actual" || $action == "student_data" || $action == "my-groups" || $action == "my-students" || $action == "student-record"))
+        if(($section == "careers" || $section == "students" || $section == "users" || $section == "groups" || $section == "activities" || $section == "units") && ($action == "add" || $action == "list" || $action == "delete" || $action == "edit"))
+        {
+            $url = "views/".$section."/".$action.".php";
+        }
+        elseif($section == "sessions" && ($action == "actual" || $action == "student_data" || $action == "add"))
+        {
+            $url = "views/".$section."/".$action.".php";
+        }
+        elseif($section == "groups" && ($action == "students" || $action == "add-student" || $action == "del-student" || $action == "my-groups" || $action == "my-students" || $action == "student-record"))
         {
             $url = "views/".$section."/".$action.".php";
         }
